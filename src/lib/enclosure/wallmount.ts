29c55b8b7429f422090de46f561a15557f73c00e
Bruce Borrett <oceativa@gmail.com>
2023-10-02 09:56:12 +0200

    Fixed wall mounts.

--- src/lib/enclosure/wallmount.ts
+++ src/lib/enclosure/wallmount.ts
@@ -6,33 +6,61 @@ import { rotateY, mirrorX } from "@jscad/modeling/src/operations/transforms"
 
 import { Params } from "../params"
 
-export const flange = (diameter: number) => {
+const SCREWCLEARANCE = 2
+const RIDGEWIDTH = 2
+const FLOOR = 2
+
+export const flange = (screwDiameter: number) => {
+  const outerWidth = screwDiameter+(SCREWCLEARANCE*2)+(RIDGEWIDTH*2)
+  const innerWidth = screwDiameter+(SCREWCLEARANCE*2)
+
   const outer = hull(
-    cube({size: diameter+8}),
-    translate([-(diameter+8)/2,0,0], cylinder({height: diameter+8, radius: (diameter/2)+4}))
+    cuboid({
+      size: [outerWidth/2, outerWidth, outerWidth]
+    }),
+    translate(
+      [
+        -(outerWidth/2),0,0
+      ], 
+      cylinder({
+        height: outerWidth, 
+        radius: outerWidth/2
+      })
+    )
   )
 
   const inner = hull(
-    cuboid({size: [diameter+4,diameter+4,diameter+4]}),
-    translate([-(diameter+8)/2,0,0], cylinder({height: diameter+4, radius: (diameter/2)+2}))
+    cuboid({
+      size: [innerWidth/2, innerWidth, innerWidth]
+    }),
+    translate(
+      [
+        -(innerWidth/2)-(SCREWCLEARANCE*2),0,0
+      ], 
+      cylinder({
+        height: innerWidth, 
+        radius: innerWidth/2
+      })
+    )
   )
 
   return subtract(
     outer,
-    translate([0,0,2], inner),
-    translate([-(diameter+4),0,18], rotateY(45, cube({size: diameter+25}))),
-    translate([-(diameter+8)/2,0,0], cylinder({height: diameter+8, radius: diameter/2}))
+    translate([RIDGEWIDTH,0,FLOOR], inner),
+    translate([-outerWidth,0,outerWidth], rotateY(45, cube({size: outerWidth*2}))),
+    translate([-outerWidth/2,0,0], cylinder({height: outerWidth, radius: screwDiameter/2}))
   )
 }
 
 export const flanges = (params: Params) => {
-  const { length, width, height, cornerRadius, wallMountScrewDiameter } = params
-  const z = ((wallMountScrewDiameter+8)/2)
-  const cornerSpacing = cornerRadius + ((wallMountScrewDiameter+8)/2)
+  const { length, width, cornerRadius, wallMountScrewDiameter } = params
+  const outerWidth = wallMountScrewDiameter+(SCREWCLEARANCE*2)+(RIDGEWIDTH*2)
+  const cornerSpacing = cornerRadius+(outerWidth/2)
+  const z = outerWidth/2
   return union(
-    translate([-(cornerRadius), cornerSpacing, z], flange(wallMountScrewDiameter)),
-    translate([-cornerRadius, length-cornerSpacing, z], flange(wallMountScrewDiameter)),
-    translate([width+cornerRadius, cornerSpacing, z], mirrorX(flange(wallMountScrewDiameter))),
-    translate([width+cornerRadius, length-cornerSpacing, z], mirrorX(flange(wallMountScrewDiameter)))
+    translate([-RIDGEWIDTH, cornerSpacing, z], flange(wallMountScrewDiameter)),
+    translate([-RIDGEWIDTH, length-cornerSpacing, z], flange(wallMountScrewDiameter)),
+    translate([width+RIDGEWIDTH, cornerSpacing, z], mirrorX(flange(wallMountScrewDiameter))),
+    translate([width+RIDGEWIDTH, length-cornerSpacing, z], mirrorX(flange(wallMountScrewDiameter)))
   )
 }
