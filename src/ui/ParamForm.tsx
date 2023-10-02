2941bb0988c92a77acbc7c3a61240c990c145326
Bruce Borrett <oceativa@gmail.com>
2023-10-02 14:58:07 +0200

    Converted config menu to accordian style.

--- src/ui/ParamForm.tsx
+++ src/ui/ParamForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { useParams } from "../lib/params";
 
 const NumberInput = ({label, value, min=undefined, onChange}: {label: string, value: number, min?: number, onChange: (e: React.ChangeEvent<HTMLInputElement| HTMLSelectElement>) => void}) => {
@@ -21,6 +23,24 @@ const CheckBox = ({label, value, onChange}: {label: string, value: boolean, onCh
   </div>
 )
 
+const Accordian = ({children, title, active, onClick}: {children: React.ReactNode, title: string, active: boolean, onClick: () => void}) => {
+  return (
+    <div className={`accordian ${active ? 'active' : ''}`}>
+      <div className="accordian-header" onClick={onClick}>
+        <p>
+          {title}
+          <span className="accordian-icon">
+            {`${active ? '-' : '+'}`}
+          </span>
+        </p>
+      </div>
+      <div className="accordian-body">
+        {children}
+      </div>
+    </div>
+  )
+}
+
 export const ParamsForm = () => {
   const { length, width, height, floor, roof, wall, cornerRadius, wallMountScrewDiameter, 
     cableGlands, cableGlandSpecs, pcbMounts, pcbMountScrewDiameter, pcbMountXY, wallMounts, 
@@ -37,93 +57,113 @@ export const ParamsForm = () => {
     document.body.click()
   }
 
+  const [activeTab, setActiveTab] = useState(1)
+
   return (
     <form id="param-form" onSubmit={handleSubmit}>
-      <NumberInput label="Length" value={length.value} min={1} onChange={(e) => handleChange(e, length.set)} />
-      <NumberInput label="Width" value={width.value} min={1} onChange={(e) => handleChange(e, width.set)} />
-      <NumberInput label="Height" value={height.value} min={1} onChange={(e) => handleChange(e, height.set)} />
-      <NumberInput label="Floor Thickness" value={floor.value} min={1} onChange={(e) => handleChange(e, floor.set)} />
-      <NumberInput label="Wall Thickness" value={wall.value} min={1} onChange={(e) => handleChange(e, wall.set)} />
-      <NumberInput label="Lid Thickness" value={roof.value} min={1} onChange={(e) => handleChange(e, roof.set)} />
-      <NumberInput label="Insert Thickness" value={insertThickness.value} min={1} onChange={(e) => handleChange(e, insertThickness.set)} />
-      <NumberInput label="Insert Height" value={insertHeight.value} min={1} onChange={(e) => handleChange(e, insertHeight.set)} />
-      <NumberInput label="Insert Clearance" value={insertClearance.value} min={1} onChange={(e) => handleChange(e, insertClearance.set)} />
-      <NumberInput label="Corner Radius" value={cornerRadius.value} min={1} onChange={(e) => handleChange(e, cornerRadius.set)} />
-      <hr />
-      <NumberInput label="Holes" value={cableGlands.value} min={0} onChange={(e) => {
-        const value = parseFloat(e.currentTarget.value)
-        cableGlandSpecs.set(Array.from({ length: value }, () => [0, 12.5]))
-        cableGlands.set(value)        
-      }} />
-      {cableGlands.value > 0 && 
-        cableGlandSpecs.map((_, i) => (
-          <div key={i}>
-            <div className="input-group">
-              <label>Hole {i+1} Wall</label>
-              <select value={_[0].value} onChange={(e) => handleChange(e, _[0].set)}>
-                <option value={0}>Front</option>
-                <option value={1}>Right</option>
-                <option value={2}>Back</option>
-                <option value={3}>Left</option>
-              </select>
-            </div>          
-            <NumberInput label={`Hole ${i+1} Diameter`} value={_[1].value} onChange={(e) => handleChange(e, _[1].set)} />
-          </div>
-        ))
-      }
-      <hr />
-      <NumberInput label="PCB Mounts" value={pcbMounts.value} min={0} onChange={(e) => {
-        const value = parseFloat(e.currentTarget.value)
-        pcbMountXY.set(Array.from({ length: value }, () => [0, 0]))
-        pcbMounts.set(value)
-      }} />
-      {
-        pcbMounts.value > 0 &&
-          <NumberInput label="Screw Diameter" value={pcbMountScrewDiameter.value} min={0} onChange={(e) => handleChange(e, pcbMountScrewDiameter.set)} />
-      }
 
-      {pcbMounts.value > 0 &&
-        pcbMountXY.map((_, i) => (
-          <div key={i}>
-            <NumberInput label={`PCB Mount ${i + 1} X`} value={_[0].value} onChange={(e) => handleChange(e, _[0].set)} />
-            <NumberInput label={`PCB Mount ${i + 1} Y`} value={_[1].value} onChange={(e) => handleChange(e, _[1].set)} />
-          </div>
-        ))
-      }
-      <hr />
-      <div className="input-group">
-        <label>Waterproof</label>
-        <input type="checkbox" id="waterProof" checked={waterProof.value} onChange={(e) => {
-          waterProof.set(e.currentTarget.checked)
-          e.currentTarget.checked && screws.set(true)
+      <Accordian title="General" active={activeTab === 1} onClick={() => setActiveTab(1)}>
+        <NumberInput label="Length" value={length.value} min={1} onChange={(e) => handleChange(e, length.set)} />
+        <NumberInput label="Width" value={width.value} min={1} onChange={(e) => handleChange(e, width.set)} />
+        <NumberInput label="Height" value={height.value} min={1} onChange={(e) => handleChange(e, height.set)} />
+        <NumberInput label="Floor Thickness" value={floor.value} min={1} onChange={(e) => handleChange(e, floor.set)} />
+        <NumberInput label="Wall Thickness" value={wall.value} min={1} onChange={(e) => handleChange(e, wall.set)} />
+        <NumberInput label="Lid Thickness" value={roof.value} min={1} onChange={(e) => handleChange(e, roof.set)} />
+        <NumberInput label="Corner Radius" value={cornerRadius.value} min={1} onChange={(e) => handleChange(e, cornerRadius.set)} />
+      </Accordian>
+
+      <Accordian title="Lid Insert" active={activeTab === 2} onClick={() => setActiveTab(2)}>
+        <NumberInput label="Insert Thickness" value={insertThickness.value} min={1} onChange={(e) => handleChange(e, insertThickness.set)} />
+        <NumberInput label="Insert Height" value={insertHeight.value} min={1} onChange={(e) => handleChange(e, insertHeight.set)} />
+        <NumberInput label="Insert Clearance" value={insertClearance.value} min={1} onChange={(e) => handleChange(e, insertClearance.set)} />
+      </Accordian>
+
+      <Accordian title="Cable Glands" active={activeTab === 3} onClick={() => setActiveTab(3)}>
+        <NumberInput label="Holes" value={cableGlands.value} min={0} onChange={(e) => {
+          const value = parseFloat(e.currentTarget.value)
+          cableGlandSpecs.set(Array.from({ length: value }, () => [0, 12.5]))
+          cableGlands.set(value)        
         }} />
-      </div>
-      {
-        waterProof.value &&
-          <NumberInput label="Seal Thickness" value={sealThickness.value} min={1} onChange={(e) => handleChange(e, sealThickness.set)} />
-      }
-      <hr />
-      <div className="input-group">
-        <label>Lid Screws</label>
-        <input type="checkbox" id="screws" checked={screws.value} onChange={(e) => {
-          screws.set(e.currentTarget.checked)
-          !e.currentTarget.checked && waterProof.set(false)
+        {cableGlands.value > 0 && 
+          cableGlandSpecs.map((_, i) => (
+            <div key={i}>
+              <div className="input-group">
+                <label>Hole {i+1} Wall</label>
+                <select value={_[0].value} onChange={(e) => handleChange(e, _[0].set)}>
+                  <option value={0}>Front</option>
+                  <option value={1}>Right</option>
+                  <option value={2}>Back</option>
+                  <option value={3}>Left</option>
+                </select>
+              </div>          
+              <NumberInput label={`Hole ${i+1} Diameter`} value={_[1].value} onChange={(e) => handleChange(e, _[1].set)} />
+            </div>
+          ))
+        }
+      </Accordian>
+
+      <Accordian title="PCB Mounts" active={activeTab === 4} onClick={() => setActiveTab(4)}>
+        <NumberInput label="PCB Mounts" value={pcbMounts.value} min={0} onChange={(e) => {
+          const value = parseFloat(e.currentTarget.value)
+          pcbMountXY.set(Array.from({ length: value }, () => [0, 0]))
+          pcbMounts.set(value)
         }} />
-      </div>
-      {
-        screws.value &&
-          <NumberInput label="Screw Diameter" value={screwDiameter.value} min={1} onChange={(e) => handleChange(e, screwDiameter.set)} />
-      }
-      <hr />
-      <div className="input-group">
-        <label>Wall Mounts</label>
-        <input type="checkbox" id="wallMounts" checked={wallMounts.value} min={1} onChange={(e) => wallMounts.set(e.currentTarget.checked)} />
-      </div>
-      {
-        wallMounts.value &&
-          <NumberInput label="Screw Diameter" value={wallMountScrewDiameter.value} min={1} onChange={(e) => handleChange(e, wallMountScrewDiameter.set)} />
-      }
+        {
+          pcbMounts.value > 0 &&
+            <NumberInput label="Screw Diameter" value={pcbMountScrewDiameter.value} min={0} onChange={(e) => handleChange(e, pcbMountScrewDiameter.set)} />
+        }
+
+        {pcbMounts.value > 0 &&
+          pcbMountXY.map((_, i) => (
+            <div key={i}>
+              <NumberInput label={`PCB Mount ${i + 1} X`} value={_[0].value} onChange={(e) => handleChange(e, _[0].set)} />
+              <NumberInput label={`PCB Mount ${i + 1} Y`} value={_[1].value} onChange={(e) => handleChange(e, _[1].set)} />
+            </div>
+          ))
+        }
+      </Accordian>
+
+      <Accordian title="Waterproofing" active={activeTab === 5} onClick={() => setActiveTab(5)}>
+        <div className="input-group">
+          <label>Waterproof</label>
+          <input type="checkbox" id="waterProof" checked={waterProof.value} onChange={(e) => {
+            waterProof.set(e.currentTarget.checked)
+            e.currentTarget.checked && screws.set(true)
+          }} />
+        </div>
+        {
+          waterProof.value &&
+            <NumberInput label="Seal Thickness" value={sealThickness.value} min={1} onChange={(e) => handleChange(e, sealThickness.set)} />
+        }
+      </Accordian>
+
+      <Accordian title="Lid Screws" active={activeTab === 6} onClick={() => setActiveTab(6)}>
+        <div className="input-group">
+          <label>Lid Screws</label>
+          <input type="checkbox" id="screws" checked={screws.value} onChange={(e) => {
+            screws.set(e.currentTarget.checked)
+            !e.currentTarget.checked && waterProof.set(false)
+          }} />
+        </div>
+        {
+          screws.value &&
+            <NumberInput label="Screw Diameter" value={screwDiameter.value} min={1} onChange={(e) => handleChange(e, screwDiameter.set)} />
+        }
+      </Accordian>
+
+      <Accordian title="Wall Mounts" active={activeTab === 7} onClick={() => setActiveTab(7)}>
+        <div className="input-group">
+          <label>Wall Mounts</label>
+          <input type="checkbox" id="wallMounts" checked={wallMounts.value} min={1} onChange={(e) => wallMounts.set(e.currentTarget.checked)} />
+        </div>
+        {
+          wallMounts.value &&
+            <NumberInput label="Screw Diameter" value={wallMountScrewDiameter.value} min={1} onChange={(e) => handleChange(e, wallMountScrewDiameter.set)} />
+        }
+      </Accordian>
+
       <button type="submit" className="hidden">Submit</button>
+
     </form>
   );
 };
