6b2c318bd35349f87dbdcd5194cc5c158872c576
Bruce Borrett <oceativa@gmail.com>
2025-09-10 16:09:56 +0200

    Disabled web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './ui/App';
-import reportWebVitals from './lib/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./ui/App";
+// import reportWebVitals from './lib/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
@@ -14,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
