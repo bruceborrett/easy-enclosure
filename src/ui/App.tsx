cd024ba2feaba4e6dc9bbf41c38244705f72f1b2
Bruce Borrett <oceativa@gmail.com>
2026-03-23 17:06:45 +0200

    Display app version in UI

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import { Funding } from "./Funding";
+import { LoadingIndicator } from "./LoadingIndicator";
+import { ParamsForm } from "./ParamForm";
+import { Renderer } from "./Renderer";
+import { Tools } from "./Tools";
 
-import { Renderer } from './Renderer';
-import { ParamsForm } from './ParamForm';
-import { Tools } from './Tools';
-import { Funding } from './Funding';
-import { LoadingIndicator } from './LoadingIndicator';
-
-import '../ui/css/main.css';
-import '../ui/css/modal.css'
-import '../ui/css/tools.css'
-import '../ui/css/funding.css'
-import '../ui/css/param-form.css'
-import '../ui/css/loading.css'
+import "../ui/css/funding.css";
+import "../ui/css/loading.css";
+import "../ui/css/main.css";
+import "../ui/css/modal.css";
+import "../ui/css/param-form.css";
+import "../ui/css/tools.css";
 
 function App() {
+  const appVersion = process.env.REACT_APP_VERSION;
+
   return (
     <>
       <Renderer />
@@ -21,8 +21,9 @@ function App() {
       <Tools />
       <Funding />
       <LoadingIndicator />
+      {appVersion && <div className="ui-version">v{appVersion}</div>}
     </>
-  )
+  );
 }
 
 export default App;
